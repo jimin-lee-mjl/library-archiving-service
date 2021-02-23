cfe4e956b07adbc9de1756d771486dff62253511
jimin <jiminaleejung@gmail.com>
2021-02-23 15:59:05 +0900

    connected db and created user table

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# any extensions using app as current_app should be inside
+with app.app_context():
+    from db import init_db
+    init_db()
+
+if __name__ == '__main__':
+    app.run
